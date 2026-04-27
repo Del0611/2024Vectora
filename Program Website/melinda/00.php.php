cb3ec3c2c1102749de7880ba43117e1f1d81fabc
melinda735 <melindachristin2@gmail.com>
2026-04-27 15:28:40 +0800

    Create 00.php.php

---
+++ Program Website/melinda/00.php.php
@@ -0,0 +1,84 @@
+<?php
+echo "Hello world!\n";
+$x = 5;
+$y = "jhon";
+echo $x;
+echo "\n";
+$b = 5.34;
+echo $b = 5.34;
+$c = false;
+echo $c;
+$t = 14;
+echo "\n";
+if ($t == 14) {
+    echo "Have a good day!\n";
+}
+
+$linda = 14;
+if ($linda === 14) {
+    echo "Have a good day!\n";
+}
+
+$q = 14;
+if ($q != 14) {
+    echo "Have a good day!\n";
+}
+
+$a = 14;
+if ($a <> 14) {
+    echo "Have a good day!\n";
+}
+
+$e = 14;
+if ($e !== 14) {
+    echo "Have a good day!\n";
+}
+
+$q = 14;
+if ($q > 14) {
+    echo "Have a good day!\n";
+}
+
+$r = 14;
+if ($r < 14) {
+    echo "Have a good day!\n";
+}
+
+$s = 14;
+if ($s >= 14) {
+    echo "Have a good day!\n";
+}
+
+$u = 14;
+if ($u <= 14) {
+    echo "Have a good day!\n";
+}
+
+$v = 14;
+if ($v <=> 14) {
+    echo "Have a good day!\n";
+}
+
+$favcolor = "red";
+switch ($favcolor) {
+case "red":
+    echo "Your favorite color is red!\n";
+    break;
+    
+    case "blue":
+    echo "Your favorite color is blue!";
+    break;
+    
+    case "green":
+    echo "Your favorite color is green!";
+    break;
+    default:
+    echo "Your favorite color is neither red, blue, nor green!";
+}
+
+$i = 1; 
+while ($i < 6) {
+  echo $i; 
+  $i++;
+}
+?>
